04a8a3d57162cff9044dffefde17a1f70195cff1
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-11-22 15:38:04 +0300

    lab4 added UPDATE, DELETE

--- lab4/load_db_by_driver.py
+++ lab4/load_db_by_driver.py
@@ -32,16 +32,37 @@ WHERE Name IN (N'Монтажглав', N'Газспец')
 """)
 show_rows(cursor)
 
-print("Выбрать участников договоров, которые зарегистрированы в Тольятти")
+print("Выбрать продавцов, которые зарегистрированы в Тольятти")
 cursor.execute("""
 SELECT s.Name AS N'Продавец'
-FROM [notary_agreements].[dbo].[Sellers] s JOIN
-    (SELECT Name AS N'Покупатель'
-    FROM [notary_agreements].[dbo].[Buyers]
-    --WHERE Town=N'Тольятти'
-    ) AS b ON s.Town=b.Town
---WHERE s.Town=N'Тольятти'
+FROM [notary_agreements].[dbo].[Sellers] s 
+WHERE s.Town=N'Тольятти'
 """)
 show_rows(cursor)
+
+cnxn.commit()
+
+print("Заменить фамилии нотариусов с Иванов на Иванов-Сергеев")
+cursor.execute("""
+UPDATE [notary_agreements].[dbo].[Notaries]
+SET Name=N'Иванов-Сергеев'
+WHERE Name LIKE '%Иванов%'
+""")
+print("Изменено строк: ", cursor.rowcount)
+cnxn.commit()
+
+
+print("Удалить 10% договоров без номеров")
+cursor.execute("""
+DELETE TOP (10) PERCENT
+FROM [notary_agreements].[dbo].[Agreements]
+WHERE [AgreementNumber] IS NULL
+""")
+print("Удалено строк: ", cursor.rowcount)
+cnxn.commit()
+
+
+
+
 
 
