6f6bed58429fb26847b110a7ed1d68f20a439533
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-11-23 07:53:17 +0300

    lab 4 done

--- lab4/load_db_by_driver.py
+++ lab4/load_db_by_driver.py
@@ -19,46 +19,54 @@ show_rows(cursor)
 
 print("Выбрать 20 первых продавцов")
 cursor.execute("""
-SELECT TOP 20 *
-FROM [notary_agreements].[dbo].[Sellers] 
-""")
+    SELECT TOP 20 *
+    FROM [notary_agreements].[dbo].[Sellers] 
+    """)
 show_rows(cursor)
 
 print("Выбрать всех покурателей с названием ""Монтажглав"" либо ""Газспец"" ")
 cursor.execute("""
-SELECT *
-FROM [notary_agreements].[dbo].[Buyers]
-WHERE Name IN (N'Монтажглав', N'Газспец') 
-""")
+    SELECT *
+    FROM [notary_agreements].[dbo].[Buyers]
+    WHERE Name IN (N'Монтажглав', N'Газспец') 
+    """)
 show_rows(cursor)
 
 print("Выбрать продавцов, которые зарегистрированы в Тольятти")
 cursor.execute("""
-SELECT s.Name AS N'Продавец'
-FROM [notary_agreements].[dbo].[Sellers] s 
-WHERE s.Town=N'Тольятти'
-""")
+    SELECT s.Name AS N'Продавец'
+    FROM [notary_agreements].[dbo].[Sellers] s 
+    WHERE s.Town=N'Тольятти'
+    """)
 show_rows(cursor)
 
 cnxn.commit()
 
-print("Заменить фамилии нотариусов с Иванов на Иванов-Сергеев")
+print("Заменить фамилии нотариусов с Иванов(а) на Иванов-Сергеев")
 cursor.execute("""
-UPDATE [notary_agreements].[dbo].[Notaries]
-SET Name=N'Иванов-Сергеев'
-WHERE Name LIKE '%Иванов%'
-""")
-print("Изменено строк: ", cursor.rowcount)
+    UPDATE [notary_agreements].[dbo].[Notaries]
+    SET Name=N'Иванов-Сергеев'
+    WHERE Name LIKE '%Иванов%'
+    """)
+print("Изменено строк: ", cursor.rowcount, "\n")
+cnxn.commit()
+
+print("Вставить новый договор")
+cursor.execute("""
+    INSERT [notary_agreements].[dbo].[Agreements] ([SellerID], [BuyerID] ,[NotaryID] ,[Date] ,[AgreementNumber])
+    VALUES( 567, 345, 941, '2019-11-23', '34/679')
+    """)
+print("Вставлено строк: ", cursor.rowcount, "\n")
 cnxn.commit()
 
 
 print("Удалить 10% договоров без номеров")
 cursor.execute("""
-DELETE TOP (10) PERCENT
-FROM [notary_agreements].[dbo].[Agreements]
-WHERE [AgreementNumber] IS NULL
-""")
-print("Удалено строк: ", cursor.rowcount)
+    DELETE TOP (10) PERCENT
+    FROM [notary_agreements].[dbo].[Agreements]
+    WHERE [AgreementNumber] IS NULL
+    """)
+print("Удалено строк: ", cursor.rowcount, "\n")
 cnxn.commit()
 
 
